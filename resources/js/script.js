7444a5c67ec9fac3743576ae6d248f1143e47d48
Wan <yuxi.wan@gmail.com>
2018-01-21 12:53:03 +0800

    Loading jquery and its test code

---
+++ resources/js/script.js
@@ -0,0 +1,14 @@
+$(document).ready(function () {
+
+    console.log("Done.");
+
+    $("h2").css("border", "2px solid #00ff00");
+
+    $(".works-step").css("border", "2px solid #ff0000");
+
+
+    $('h1').click(function () {
+        $(this).css("border", "5px solid #0000ff");
+
+    })
+})
